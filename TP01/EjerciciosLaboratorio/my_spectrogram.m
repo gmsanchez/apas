b8d345960f01556382b0ef609e67138bab4910f8
Guido Sanchez <sanchez.guido@gmail.com>
2013-12-16 14:37:42 -0300

    Termino espectrograma.

--- TP01/EjerciciosLaboratorio/my_spectrogram.m
+++ TP01/EjerciciosLaboratorio/my_spectrogram.m
@@ -20,25 +20,33 @@ end
 
 ts = 1/fs;
 n = length(x);
-wl05 = floor(wlength/2);
-x = [zeros(1,wlength) x zeros(1,wlength)];
+wl05c =  ceil(wlength/2);
+wl05f = floor(wlength/2);
+%x = [zeros(1,wlength) x zeros(1,wlength)];
 w = window(fhandle,wlength)';
-specgram = zeros(wl05,n);
 nw = floor(n/wspacing); % cantidad de ventanas que utilizo para 'ventanear'
-
-for i=1:1:nw
-    t = wlength + 1 + (i-1)*wspacing;
-    tmin = t-ceil(wlength/2);
-    tmax = tmin+wlength-1;
-    xloc = x(tmin:tmax).*w;
+specgram = ones(wl05f,nw+1);
+s = 1;
+for i=1:wspacing:n
+    tmin = i-wl05c;
+    tmax = i+wl05f-1;
+    if tmin<1
+        xloc = [zeros(1,-tmin+1) x(1:tmax)];
+    elseif tmax>n
+        xloc = [x(tmin:n) zeros(1,tmax-n)];
+    else
+        xloc = x(tmin:tmax);
+    end
+    xloc = xloc.*w;
     specloc = abs(fft(xloc));
-    specgram(:,t-wlength) = specloc(1:wl05);
+    specgram(:,s) = specloc(1:wl05f);
+    s = s+1;
 end
 
 colormap(jet)
 taxis = linspace(0,ts*(n-1),1/ts);
 deltaf = fs/wlength;
-faxis = linspace(0,deltaf*wl05,wl05);
+faxis = linspace(0,deltaf*wl05f,wl05f);
 imagesc(taxis,faxis,specgram)
 axis('xy')
 xlabel('Time')
