d2613185a483b3caca29e3ee7a27dce8d2ccb2bd
Guido Sanchez <sanchez.guido@gmail.com>
2013-12-17 17:46:39 -0300

    Sigo con el ejercicio 3, guia 2.

--- TP02/EjerciciosLaboratorio/tp02_ej03.m
+++ TP02/EjerciciosLaboratorio/tp02_ej03.m
@@ -1,5 +1,25 @@
-%% Guia 2 - Ejercicio 2
+%% Guia 2 - Ejercicio 3
 %
 %% Enunciado:
 % Escriba un programa en Matlab que permita visualizar la convergencia de
 % la fórmula de Poisson antes mencionada.
+
+close all;
+clear all;
+
+T = 2;
+n = 20;
+w = -4*pi/T:0.001:4*pi/T;
+p0 = 0.*w;
+
+N = [1 10 100 1000];
+lN = length(N);
+for l=1:lN
+    p0 = 0.*w;
+    for k=-N(l):1:N(l)
+        p0 = p0 + exp(-1i*k*T.*w);
+    end
+    subplot(lN,1,l); plot(w,p0);
+    title(sprintf('n=%d',N(l)));
+end
+
