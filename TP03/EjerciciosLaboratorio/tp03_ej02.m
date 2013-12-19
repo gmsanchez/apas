bda16c2864565c1f449a932a1d79011f19d5747e
Guido Sanchez <sanchez.guido@gmail.com>
2013-12-19 17:12:47 -0300

    Termino ejercicio 2, guia 3.

--- TP03/EjerciciosLaboratorio/tp03_ej02.m
+++ TP03/EjerciciosLaboratorio/tp03_ej02.m
@@ -1,22 +1,43 @@
 %% Guia 3 - Ejercicio 2
 %
 
+%% Enunciado
+% Utilice la función wavefun para explorar las formas de las funciones 
+% ondita y de escala para la familia Daubechies 6. Utilize 2, 3 y 4 
+% iteraciones. Para cada funció (de escala y ondita) y para cada numero 
+% de iteraciones, grafique tambien la magnitud de la transformada de 
+% Fourier de las mismas.
+
 clear all;
 close all;
 
 
 % Nombre de la ondita y cantidad de iteraciones
-wav = 'db6'
+wav = 'db6';
 iter = [2 3 4];
 N = length(iter);
 
 for i = 1:N
     [phi,psi,xval] = wavefun(wav,iter(i));
-    figure(1)
-    subplot(N,2,2*(i-1)+1),plot(xval,psi);
-    subplot(N,2,2*(i-1)+2),plot(fftshift(abs(fft(psi))));
-    figure(2)
-    subplot(N,2,2*(i-1)+1),plot(xval,phi);
-    subplot(N,2,2*(i-1)+2),plot(fftshift(abs(fft(phi))));
+    xvalf = linspace(-pi,pi,length(xval));
+    figure(i)
+    
+    % Ondita
+    subplot(2,2,1), plot(xval,psi);
+    title(sprintf('Ondita %s - Iter %d',wav,iter(i)));
+    axis('tight')
+    subplot(2,2,3), plot(xvalf,fftshift(abs(fft(psi))));
+    title(sprintf('Espectro Mag. Ondita %s - Iter %d',...
+        wav,iter(i)));
+    axis('tight')
+    
+    % Scaling
+    subplot(2,2,2), plot(xval,phi);
+    title(sprintf('Scaling %s - Iter %d',wav,iter(i)));
+    axis('tight')
+    subplot(2,2,4), plot(xvalf,fftshift(abs(fft(phi))));
+    title(sprintf('Espectro Mag. Scaling %s - Iter %d',...
+        wav,iter(i)));
+    axis('tight')
 end
 
