a2a39b0da59a31b985a33a0faa98252734ee2fbe
Guido Sanchez <sanchez.guido@gmail.com>
2013-12-27 15:22:35 -0300

    Completo ejercicio 2, faltan comentarios.

--- TP04/tp04_ej02.m
+++ TP04/tp04_ej02.m
@@ -32,3 +32,25 @@ subplot(3,1,1),stem(xh);
 subplot(3,1,2),stem(my_cconv(xh,h));
 subplot(3,1,3),stem(my_cconv(xh,g));
 
+
+%% DB4
+[h, g] = db4(1);
+
+%% Respuesta en frecuencia
+
+w = 0:2*pi/200:2*pi;
+Hw = 0.*w;
+for k=1:length(h)
+    Hw = Hw + h(k)*exp(-1i*w*(k-1));
+end
+
+Gw = 0.*w;
+for k=1:length(g)
+    Gw = Gw + g(k)*exp(-1i*w*(k-1));
+end
+
+% Verifico ecuacion 7e
+eq7e = abs(Hw).^2+abs(Gw).^2;
+
+figure()
+plot(w,abs(Hw),'r',w,abs(Gw),'b',w,eq7e,'g');
