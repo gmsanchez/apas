0803cd65e37c6dee84585f5d186ba9aa88e84903
Guido Sanchez <sanchez.guido@gmail.com>
2014-01-22 11:57:57 -0300

    Se agrega BP lineal.

---
+++ TP05/my_lbp.m
@@ -0,0 +1,18 @@
+function [ a_hat ] = my_lbp( x,phi )
+%MY_LBP Summary of this function goes here
+%   Detailed explanation goes here
+    Al = [phi -phi];
+    a0 = Al'*x;
+    lb = 0.*a0;
+    ub = [];
+    
+    c = ones(size(a0));
+    al_hat = linprog(c,[],[],Al,x,lb,ub,a0);
+    
+    luv = size(phi,2);
+    u = al_hat(1:luv);
+    v = al_hat(luv+1:end);
+    a_hat = u-v;
+    
+end
+
