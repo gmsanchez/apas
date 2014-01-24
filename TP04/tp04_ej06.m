efff46384be5e33ffa90f3c90dc414baf36b9d3d
Guido Sanchez <sanchez.guido@gmail.com>
2014-01-24 20:19:16 -0300

    Guia 4, Ejercicio 6

--- TP04/tp04_ej06.m
+++ TP04/tp04_ej06.m
@@ -24,3 +24,15 @@ else
     title(sprintf('DPOC %d niveles.',k));
 end
 end
+
+%% WPDEC
+terminalnodes = [7 8 4 5 13 14];
+[c0,t0] =  my_wpdec(y,Lo_D,Hi_D,3,terminalnodes);
+
+%% Reconstruccion
+yr = my_wprec(c0,Lo_R,Hi_R,t0);
+
+figure();
+subplot(3,1,1),plot(y);
+subplot(3,1,2),plot(yr);
+subplot(3,1,3),plot(yr-y);
