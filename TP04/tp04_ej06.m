e4331808db32f54b1726ca3179bb191a173d0cf9
Guido Sanchez <sanchez.guido@gmail.com>
2014-01-26 16:21:53 -0300

    Se va emprolijando la guia 4.

--- TP04/tp04_ej06.m
+++ TP04/tp04_ej06.m
@@ -6,7 +6,11 @@ clear all;
 % Obtenemos nuestra senoidal
 get_senoidal
 
-%% Inciso a
+%% Inciso a) Escriba una función que permita calcular la descomposición 
+% Paquetes de Onditas completa (es decir sobre el árbol completo), iterando 
+% con la función dstep tanto sobre los coeficientes de aproximación como en 
+% los de escala, hasta el nivel deseado.
+
 [Lo_D,Hi_D,Lo_R,Hi_R] = db4(1);
 lvl = 4;
 
@@ -25,14 +29,55 @@ else
 end
 end
 
-%% WPDEC
+%% Inciso b) Escriba una una función wpdec que permita ingresar la 
+% estructura del árbol de descomposición deseado y devuelva los 
+% coeficientes de la descomposición obtenida, iterando la función dstep 
+% sobre los coeficientes adecuados.
+
+dbtype my_wpdec;
+
+%% Inciso c) Escriba una función wprec que reconstruya mediante la 
+% iteración de la función rstep, una señal, dada su descomposición generada 
+% con wpdec y la estructura del árbol.
+
+dbtype my_wprec
+
+%% Inciso d) Utilice esta descomposición y reconstrucción sobre una señal 
+% cualquiera. Grafique la señal original, la reconstruida y el error de 
+% reconstrucción. Utilice los filtros Haar y Daubechies normalizados.
+Lo_D = cell(1,2); Hi_D = cell(1,2);
+Lo_R = cell(1,2); Hi_R = cell(1,2);
+
+[Lo_D{1},Hi_D{1},Lo_R{1},Hi_R{1}] = haar(1);
+[Lo_D{2},Hi_D{2},Lo_R{2},Hi_R{2}] = db4(1);
+wav{1} = 'Haar';
+wav{2} = 'DB2';
+
 terminalnodes = [7 8 4 5 13 14];
-[c0,t0] =  my_wpdec(y,Lo_D,Hi_D,3,terminalnodes);
 
-%% Reconstruccion
-yr = my_wprec(c0,Lo_R,Hi_R,t0);
+for k = 1:length(Lo_D)
+    fprintf('===== %s =====\n',wav{k});
+    % Descomposicion
+    [c0,t0] =  my_wpdec(y,Lo_D{k},Hi_D{k},3,terminalnodes);
+    plot(t0);
+    figure();
+    plot(c0); title(sprintf('Coeficientes generados con %s',wav{k}));
 
-figure();
-subplot(3,1,1),plot(y);
-subplot(3,1,2),plot(yr);
-subplot(3,1,3),plot(yr-y);
+    % Reconstruccion
+    yr = my_wprec(c0,Lo_R{k},Hi_R{k},t0);
+    figure();
+    subplot(3,1,1),plot(y);
+    title('Se~nal original');
+    subplot(3,1,2),plot(yr);
+    title('Se~nal reconstruida');
+    subplot(3,1,3),plot(yr-y);
+    title(sprintf('Error de reconstruccion utilizando %s',wav{k}));
+end
+
+%% Anexo: Código fuente de funciones utilizadas
+
+%% get_senoidal
+dbtype get_senoidal.m
+
+%% dpoc
+dbtype dpoc.m
--- TP04/tp04_ej07.m
+++ TP04/tp04_ej07.m
@@ -4,16 +4,49 @@ close all;
 clear all;
 
 N = 2^7;
-c = zeros(N,1);
-c(2) = 1;
 % Supongo descomposición TODD completa de nivel 5
 t = ntree(2,5);
+
+%% Inciso a) Genere una señal con todos ceros de longitud N = 2^7. Suponga 
+% que ésta es el resultado de una descomposición TODD hasta nivel 5, que 
+% tiene los coeficientes [a5d5d4d3d2d1]. En la posición correspondiente a 
+% a5[2] (en el vector total será la posición 2) coloque un 1. Reconstruya 
+% usando la TODD inversa, y grafique la señal obtenida. Utilice para ello 
+% los filtros de Haar y los Daubechies. Esto permite obtener la función de 
+% escala para dichos filtros, compárelas con las que aparecen en la 
+% bibliografía (por ejemplo, en la pagina 23 de [2] para la Haar, y en 
+% página 253 de [1] para la Daubechies con p = 2 momentos desvanecientes).
+c = zeros(N,1);
+c(2) = 1;
+
 % Filtros de Haar
 [Lo_D,Hi_D,Lo_R,Hi_R] = haar(1);
 y = my_wprec(c,Lo_R,Hi_R,t);
-figure(),plot(y)
+figure(),plot(y), axis normal, grid on;
 
 % Filtros DB2
 [Lo_D,Hi_D,Lo_R,Hi_R] = db4(1);
 y = my_wprec(c,Lo_R,Hi_R,t);
-figure(),plot(y)
+figure(),plot(y), axis normal, grid on;
+
+%% Inciso b) Genere una señal con todos ceros de longitud N = 2^7. Suponga 
+% que ésta es el resultado de una descomposición TODD hasta nivel 5, que 
+% tiene los coeficientes [a5d5d4d3d2d1]. En la posición correspondiente a 
+% d5[2] (en el vector total será la posición 6) coloque un 1. Reconstruya 
+% usando la TODD inversa, y grafique la señal obtenida. Utilice para ello 
+% los filtros de Haar y los Daubechies. Esto permite obtener la función de 
+% escala para dichos filtros, compárelas con las que aparecen en la 
+% bibliografía (por ejemplo en página 24 de [2] para la Haar, y en página 
+% 253 de [1] para la Daubechies con p = 2 momentos desvanecientes).
+c = c.*0;
+c(6) = 1;
+
+% Filtros de Haar
+[Lo_D,Hi_D,Lo_R,Hi_R] = haar(1);
+y = my_wprec(c,Lo_R,Hi_R,t);
+figure(),plot(y), axis normal, grid on;
+
+% Filtros DB2
+[Lo_D,Hi_D,Lo_R,Hi_R] = db4(1);
+y = my_wprec(c,Lo_R,Hi_R,t);
+figure(),plot(y), axis normal, grid on;
